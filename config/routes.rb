e15f3816762b8ec4ecc54112b868229ad7add365
keitai <cj.miyake@gmail.com>
2018-01-06 23:25:51 -0800

    fixed admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :admin do
     resources :users
-    root to: "therapists#index"
+    root to: "users#index"
   end
   root to: 'therapists#index'
   devise_for :users
