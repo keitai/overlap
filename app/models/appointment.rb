5d2fdcdea75f9f6a46572c651cb0e5cd89357c18
keitai <cj.miyake@gmail.com>
2018-01-09 10:02:54 -0800

    cleaned up styles

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -4,7 +4,14 @@ class Appointment < ApplicationRecord
   
   
   def score
-    return [self.behavior_management_score, self.natural_environment_score, self.behavior_management_score, self.structured_lesson_score, self.language_promotion_score].sum
+    return [self.behavior_management_score, self.natural_environment_score, self.behavior_management_score, self.structured_lesson_score, self.language_promotion_score].compact.sum
   end
+  
+  def overlap_number(therapist, appointment)
+    overlaps = Appointment.where(therapist_id: therapist)
+    overlaps.index(appointment) + 1
+  end
+  
+  
   
 end
